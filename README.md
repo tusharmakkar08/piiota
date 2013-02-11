b5f083349d3dcf4cfd75ef6c7a423ee5fdb4fb0d
Tushar Makkar <tusharmakkar08@gmail.com>
2013-02-11 12:00:18 +0530

    README changed

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 An Automated Answer telling machine made using python.
 
 P.S:
-1.To access information from Wikipedia write "w:" (without double quotes) and then write your query
-2.The Unwiki code is taken from https://github.com/Anorov/Imageboard-Spammer-Deluxe/blob/d735cc24468528bb6c6cd1a1447986e550478804/wikipedia.py
-
+To access information from Wikipedia write "w:" (without double quotes) and then write your query.
--- piiotamain.py
+++ piiotamain.py
@@ -4,24 +4,19 @@ from bs4 import BeautifulSoup
 import cPickle as pickle
 class WikipediaError(Exception):
     pass
-
 class GrabArticle(object):    
     def __init__(self):
         self.url_random = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&grnnamespace=0&rvsection=0&generator=random&rvprop=content&format=xml"
-    
     def __fetch(self, url):
         request = urllib2.Request(url)
         request.add_header("User-Agent", "Mozilla/5.0")
-        
         try:
             result = urllib2.urlopen(request)
         except urllib2.HTTPError, e:
             raise WikipediaError(e.code)
         except urllib2.URLError, e:
             raise WikipediaError(e.reason)
-        
         return result
-    
     def findArticle(self, article=None):
         if article is not None:
             title=re.sub(r"\s" ,r"%20", article)
@@ -43,46 +38,34 @@ class GrabArticle(object):
                     return self.findArticle(article.title())
             else:
                 return 'Article "%s" cannot be found.' % article
-        
         try:
             if '#REDIRECT' in text:
                 match = re.match(r"(?i)#REDIRECT \[\[([^\[\]]+)\]\]", text)
-                
                 if match is not None:
                     return self.findArticle(match.group(1))
-                
                 raise WikipediaError("Can't find redirect article.")
-            
             return content
         except:
             return None
-    
     def image(self, image, thumb=None):
         url = self.url_image % (image)
         result = self.__fetch(url)
         content = result.read()
-        
         if thumb:
             url = result.geturl() + '/' + thumb + 'px-' + image
             url = url.replace('/commons/', '/commons/thumb/')
             url = url.replace('/' + self.lang + '/', '/' + self.lang + '/thumb/')
-            
             return self.__fetch(url).read()
-        
         return content
-
 class Wiki2Plain(object):
     def __init__(self, wiki):
         self.wiki = wiki
-        
         self.text = wiki
         self.text = self.unhtml(self.text)
         self.text = self.unwiki(self.text)
         self.text = self.punctuate(self.text)
-    
     def __str__(self):
         return self.text
-    
     def unwiki(self, wiki):
         """
         Remove wiki markup from the text.
@@ -107,9 +90,7 @@ class Wiki2Plain(object):
         wiki = re.sub(r'&lt;ref.*?&gt;.*?&lt;/ref.*?&gt;', '', wiki, flags=re.DOTALL)
         wiki = re.sub(r'&lt;.*?&gt;', '', wiki, flags=re.DOTALL)
         wiki = re.sub(r'&\w{1,4};', '', wiki)
-        
         return wiki
-    
     def unhtml(self, html):
         """
         Remove HTML from the text.
@@ -120,9 +101,7 @@ class Wiki2Plain(object):
         html = re.sub(r'(?i)<ref[^>]*>[^>]*<\/ ?ref>', '', html)
         html = re.sub(r'(?m)<.*?>', '', html)
         html = re.sub(r'(?i)&amp;', '&', html)
-        
         return html
-    
     def punctuate(self, text):
         """
         Convert every text part into well-formed one-space
@@ -130,36 +109,26 @@ class Wiki2Plain(object):
         """
         text = re.sub(r'\r\n|\n|\r', '\n', text)
         text = re.sub(r'\n\n+', '\n\n', text)
-        
         parts = text.split('\n\n')
         partsParsed = []
-        
         for part in parts:
             part = part.strip()
-            
             if len(part) == 0:
                 continue
-            
             partsParsed.append(part)
-        
         return '\n\n'.join(partsParsed)
-    
     def image(self):
         """
         Retrieve the first image in the document.
         """
         # match = re.search(r'(?i)\|?\s*(image|img|image_flag)\s*=\s*(<!--.*-->)?\s*([^\\/:*?<>"|%]+\.[^\\/:*?<>"|%]{3,4})', self.wiki)
         match = re.search(r'(?i)([^\\/:*?<>"|% =]+)\.(gif|jpg|jpeg|png|bmp)', self.wiki)
-        
         if match:
             return '%s.%s' % match.groups()
-        
         return None
-
 def getArticle(article):
     wiki=GrabArticle()
     raw=wiki.findArticle(article)
-
     wiki2plain=Wiki2Plain(raw)
     article=wiki2plain.text
     try:
@@ -177,6 +146,8 @@ while(1):
     print "2 . Exit"
     a=input()
     if a==2:
+        print "Thank you"
+        tts("Thank\ you")
         break
     if a==1:
         print "Welcome... Talk to me !!"
@@ -243,3 +214,4 @@ while(1):
                             foi.update(do)              # updating the dictionary
                         pickle.dump(foi,open(l,"wb"))   # saving the dictionary
                         print "Thank you"
+                        tts("Thank\ you")
